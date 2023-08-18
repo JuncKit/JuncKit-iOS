9be28479bad16ad6675d405fe8a7f54ac3af85a2
kpk0616 <kpk0616@naver.com>
2023-08-19 04:43:22 +0900

    [Feat] 로그인 성공 시 새로운 화면 연결

--- JuncKit/JuncKit/View/LogIn/LogInView.swift
+++ JuncKit/JuncKit/View/LogIn/LogInView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 
 struct LogInView: View {
-    var body: some View {
-        VStack {
-          AppleSigninButton()
-        }
-        .frame(height:UIScreen.main.bounds.height)
-        .background(Color.white)
+  @State var isSuccessLogIn = false
+  
+  var body: some View {
+    ZStack {
+      AppleSigninButton(isSuccessLogIn: $isSuccessLogIn)
+      if (isSuccessLogIn) {
+        ContentView()
+      }
     }
-    
+    .frame(height:UIScreen.main.bounds.height)
+    .background(Color.white)
+  }
+  
 }
 
 struct LogInView_Previews: PreviewProvider {
-    static var previews: some View {
-        LogInView()
-    }
+  static var previews: some View {
+    LogInView()
+  }
 }
