d0f9f0646666fc1a5958ecbe094a8b75840b3772
kpk0616 <kpk0616@naver.com>
2023-08-19 00:49:55 +0900

    [Init] 프로젝트 생성

---
+++ JuncKit/JuncKit/JuncKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuncKitApp.swift
+//  JuncKit
+//
+//  Created by 박의서 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct JuncKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
