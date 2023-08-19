e5a3c08c9e203d82a834f628c13c5f0983be32f1
kpk0616 <kpk0616@naver.com>
2023-08-19 19:52:26 +0900

    [Feat] TimePicker 구현

--- JuncKit/JuncKit/View/BusDriver/SetTimeView.swift
+++ JuncKit/JuncKit/View/BusDriver/SetTimeView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct SetTimeView: View {
+  @State private var startDate = Date()
+  @State private var endDate = Date()
+  @State private var isShowingAlert = false
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack(spacing: 0) {
+        HStack {
+          Text("오늘 하루동안\n운영가능한 시간을\n알려주세요")
+            .font(.system(size: 40))
+            .padding([.top], 33)
+          Spacer()
+        }
+        .padding([.leading, .trailing], 24)
+        HStack {
+          DatePicker("", selection: $startDate, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+          Text("부터")
+        }
+        HStack {
+          DatePicker("", selection: $endDate, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+            .onChange(of: endDate) { time in
+              if (time <= startDate) {
+                isShowingAlert.toggle()
+              }
+            }
+          Text("까지")
+        }
+        Spacer()
+        Button {
+          // 콜 받기
+        } label: {
+          Text("콜 받기")
+        }
+        .buttonStyle(MainButtonStyle())
+        .padding([.bottom], 30)
+      }
     }
 }
 
